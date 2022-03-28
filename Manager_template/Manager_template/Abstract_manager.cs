93731929f50e5672c4d9b88ec30ef87a83582895
AVazianskii <avazianskii@mail.ru>
2022-03-28 22:11:50 +0300

    added new version of common method in abstract manager class. old versions saved for saving current functionality

--- Manager_template/Manager_template/Abstract_manager.cs
+++ Manager_template/Manager_template/Abstract_manager.cs
@@ -113,6 +113,77 @@ namespace SW_Character_creation
                 }
             }
         }
+        public void Run_download_from_SQLite_v2(string SQLite_Command_text,
+                                            SQLiteConnection SQLite_connection,
+                                            List<string> Names_of_coloumns,
+                                            object List_for_reading_data,
+                                            int reading_data_type)
+        {
+            string SQLite_command_text;
+            SQLiteConnection SQLite_conn;
+            SQLiteDataReader SQLite_reader;
+            bool first_run;
+            List<List<string>> temp_string_list;
+            List<List<int>> temp_int_list;
+            List<List<bool>> temp_bool_list;
+
+            SQLite_command_text = SQLite_Command_text;
+            SQLite_conn = SQLite_connection;
+            SQLiteCommand SQLite_command = new SQLiteCommand(SQLite_command_text, SQLite_conn);
+            SQLite_reader = SQLite_command.ExecuteReader();
+
+            first_run = true;
+
+            while (SQLite_reader.Read())
+            {
+                if (first_run)
+                {
+                    for (int i = 0; i < SQLite_reader.FieldCount; i++)
+                    {
+                        Names_of_coloumns.Add(SQLite_reader.GetName(i));
+                    }
+                    Names_of_coloumns.RemoveAt(0); // удаояем название столбца ID
+                    Names_of_coloumns.RemoveAt(0); // удаояем название столбца Название расы
+                    first_run = false;
+                }
+                int index;
+                object temp_object;
+
+                foreach (string coloumn_name in Names_of_coloumns)
+                {
+                    temp_object = SQLite_reader[coloumn_name];
+                    index = Names_of_coloumns.IndexOf(coloumn_name);
+
+                    if (reading_data_type == (int)Type_of_var.string_type)
+                    {
+                        temp_string_list = (List<List<string>>)List_for_reading_data;
+                        if (!(temp_object is DBNull)) 
+                        {
+                            temp_string_list.Add(new List<string>());
+                            temp_string_list[index].Add(Convert.ToString(temp_object)); 
+                        } else { temp_string_list[index].Add(""); }
+                    }
+                    else if (reading_data_type == (int)Type_of_var.int_type)
+                    {
+                        temp_int_list = (List<List<int>>)List_for_reading_data;
+                        if (!(temp_object is DBNull)) 
+                        {
+                            temp_int_list.Add(new List<int>());
+                            temp_int_list[index].Add(Convert.ToInt32(temp_object));
+                        } else { temp_int_list[index].Add(0); }
+                    }
+                    else if (reading_data_type == (int)Type_of_var.bool_type)
+                    {
+                        temp_bool_list = (List<List<bool>>)List_for_reading_data;
+                        if (!(temp_object is DBNull)) 
+                        {
+                            temp_bool_list.Add(new List<bool>());
+                            temp_bool_list[index].Add(Convert.ToBoolean(temp_object)); 
+                        } else { temp_bool_list[index].Add(false); }
+                    }
+                }
+            }
+        }
         public virtual void Run_download_and_upload_process() { }
     }
 }
