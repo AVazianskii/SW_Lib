76659a0f2f23ba81fbc6385900bdde2ca743bfdc
avazianskii@mail.ru <avazianskii@mail.ru>
2022-04-23 22:05:48 +0500

    some code refactoring

--- Manager_template/SW_Race_management/Race_manager/05_Race_manager_Method_upload_race_skill_bonus.cs
+++ Manager_template/SW_Race_management/Race_manager/05_Race_manager_Method_upload_race_skill_bonus.cs
@@ -1,4 +1,5 @@
-﻿using Races_libs;
+﻿using System.Collections.Generic;
+using Races_libs;
 
 namespace SW_Character_creation
 {
@@ -9,56 +10,10 @@ namespace SW_Character_creation
             foreach(Race_class Race in _Races)
             {
                 int index = _Races.IndexOf(Race);
-                Race.Set_swimming_bonus             (Race_skills_bonus[Race_skills_bonus.IndexOf(Swimming_bonus)]               [index]);
-                Race.Set_heavy_athletics_bonus      (Race_skills_bonus[Race_skills_bonus.IndexOf(Heavy_athletics_bonus)]        [index]);
-                Race.Set_climbing_bonus             (Race_skills_bonus[Race_skills_bonus.IndexOf(Climbing_bonus)]               [index]);
-                Race.Set_light_athletics_bonus      (Race_skills_bonus[Race_skills_bonus.IndexOf(Light_athletics_bonus)]        [index]);
-                Race.Set_sleight_of_hand_bonus      (Race_skills_bonus[Race_skills_bonus.IndexOf(Sleight_of_hand_bonus)]        [index]);
-                Race.Set_stealthiness_bonus         (Race_skills_bonus[Race_skills_bonus.IndexOf(Stealthiness_bonus)]           [index]);
-                Race.Set_acrobatics_bonus           (Race_skills_bonus[Race_skills_bonus.IndexOf(Acrobatics_bonus)]             [index]);
-                Race.Set_horse_riding_bonus         (Race_skills_bonus[Race_skills_bonus.IndexOf(Horse_riding_bonus)]           [index]);
-                Race.Set_light_saber_fighting_bonus (Race_skills_bonus[Race_skills_bonus.IndexOf(Light_saber_fighting_bonus)]   [index]);
-                Race.Set_hand_combat_bonus          (Race_skills_bonus[Race_skills_bonus.IndexOf(Hand_combat_bonus)]            [index]);
-                Race.Set_usual_saber_fighting_bonus (Race_skills_bonus[Race_skills_bonus.IndexOf(Usual_saber_fighting_bonus)]   [index]);
-                Race.Set_evasion_bonus              (Race_skills_bonus[Race_skills_bonus.IndexOf(Evasion_bonus)]                [index]);
-                Race.Set_throwing_bonus             (Race_skills_bonus[Race_skills_bonus.IndexOf(Throwing_bonus)]               [index]);
-                Race.Set_lips_reading_bonus         (Race_skills_bonus[Race_skills_bonus.IndexOf(Lips_reading_bonus)]           [index]);
-                Race.Set_old_weapon_bonus           (Race_skills_bonus[Race_skills_bonus.IndexOf(Old_weapon_bonus)]             [index]);
-                Race.Set_light_weapon_bonus         (Race_skills_bonus[Race_skills_bonus.IndexOf(Light_weapon_bonus)]           [index]);
-                Race.Set_heavy_weapon_bonus         (Race_skills_bonus[Race_skills_bonus.IndexOf(Heavy_weapon_bonus)]           [index]);
-                Race.Set_sniper_weapon_bonus        (Race_skills_bonus[Race_skills_bonus.IndexOf(Sniper_weapon_bonus)]          [index]);
-                Race.Set_musicality_bonus           (Race_skills_bonus[Race_skills_bonus.IndexOf(Musicality_bonus)]             [index]);
-                Race.Set_riding_bonus               (Race_skills_bonus[Race_skills_bonus.IndexOf(Riding_bonus)]                 [index]);
-                Race.Set_resisting_bonus            (Race_skills_bonus[Race_skills_bonus.IndexOf(Resisting_bonus)]              [index]);
-                Race.Set_strength_of_mind_bonus     (Race_skills_bonus[Race_skills_bonus.IndexOf(Strength_of_mind_bonus)]       [index]);
-                Race.Set_watchfulness_bonus         (Race_skills_bonus[Race_skills_bonus.IndexOf(Watchfulness_bonus)]           [index]);
-                Race.Set_pilot_bonus                (Race_skills_bonus[Race_skills_bonus.IndexOf(Pilot_bonus)]                  [index]);
-                Race.Set_programming_bonus          (Race_skills_bonus[Race_skills_bonus.IndexOf(Programming_bonus)]            [index]);
-                Race.Set_surviving_bonus            (Race_skills_bonus[Race_skills_bonus.IndexOf(Surviving_bonus)]              [index]);
-                Race.Set_reapair_bonus              (Race_skills_bonus[Race_skills_bonus.IndexOf(Reapair_bonus)]                [index]);
-                Race.Set_training_bonus             (Race_skills_bonus[Race_skills_bonus.IndexOf(Training_bonus)]               [index]);
-                Race.Set_medicine_crafting_bonus    (Race_skills_bonus[Race_skills_bonus.IndexOf(Medicine_crafting_bonus)]      [index]);
-                Race.Set_science_bonus              (Race_skills_bonus[Race_skills_bonus.IndexOf(Science_bonus)]                [index]);
-                Race.Set_engineering_bonus          (Race_skills_bonus[Race_skills_bonus.IndexOf(Engineering_bonus)]            [index]);
-                Race.Set_star_wars_tactic_bonus     (Race_skills_bonus[Race_skills_bonus.IndexOf(Star_wars_tactic_bonus)]       [index]);
-                Race.Set_crafting_bonus             (Race_skills_bonus[Race_skills_bonus.IndexOf(Crafting_bonus)]               [index]);
-                Race.Set_trading_bonus              (Race_skills_bonus[Race_skills_bonus.IndexOf(Trading_bonus)]                [index]);
-                Race.Set_astronavigation_bonus      (Race_skills_bonus[Race_skills_bonus.IndexOf(Astronavigation_bonus)]        [index]);
-                Race.Set_change_looking_bonus       (Race_skills_bonus[Race_skills_bonus.IndexOf(Change_looking_bonus)]         [index]);
-                Race.Set_light_saber_crafting_bonus (Race_skills_bonus[Race_skills_bonus.IndexOf(Light_saber_crafting_bonus)]   [index]);
-                Race.Set_exploding_bonus            (Race_skills_bonus[Race_skills_bonus.IndexOf(Exploding_bonus)]              [index]);
-                Race.Set_security_bonus             (Race_skills_bonus[Race_skills_bonus.IndexOf(Security_bonus)]               [index]);
-                Race.Set_medicine_bonus             (Race_skills_bonus[Race_skills_bonus.IndexOf(Medicine_bonus)]               [index]);
-                Race.Set_language_bonus             (Race_skills_bonus[Race_skills_bonus.IndexOf(Language_bonus)]               [index]);
-                Race.Set_knowledge_bonus            (Race_skills_bonus[Race_skills_bonus.IndexOf(Knowledge_bonus)]              [index]);
-                Race.Set_dancing_bonus              (Race_skills_bonus[Race_skills_bonus.IndexOf(Dancing_bonus)]                [index]);
-                Race.Set_interrogation_bonus        (Race_skills_bonus[Race_skills_bonus.IndexOf(Interrogation_bonus)]          [index]);
-                Race.Set_diplomacy_bonus            (Race_skills_bonus[Race_skills_bonus.IndexOf(Diplomacy_bonus)]              [index]);
-                Race.Set_leadership_bonus           (Race_skills_bonus[Race_skills_bonus.IndexOf(Leadership_bonus)]             [index]);
-                Race.Set_singing_bonus              (Race_skills_bonus[Race_skills_bonus.IndexOf(Singing_bonus)]                [index]);
-                Race.Set_deception_bonus            (Race_skills_bonus[Race_skills_bonus.IndexOf(Deception_bonus)]              [index]);
-                Race.Set_insight_bonus              (Race_skills_bonus[Race_skills_bonus.IndexOf(Insight_bonus)]                [index]);
-                Race.Set_seduction_bonus            (Race_skills_bonus[Race_skills_bonus.IndexOf(Seduction_bonus)]              [index]);
+                foreach (List<int> list in Race_skills_bonus)
+                {
+                    Race.Race_skill_bonus.Add(list[index]);
+                }
             }
         }
     }
--- Manager_template/SW_Race_management/Race_template/Race_class.cs
+++ Manager_template/SW_Race_management/Race_template/Race_class.cs
@@ -72,60 +72,7 @@ namespace Races_libs
                     watchfulness_combat_bonus,
                     flow_control_bonus;
 
-        private int swimming_bonus,
-                    heavy_athletics_bonus,
-                    climbing_bonus,
-                    light_athletics_bonus,
-                    sleight_of_hand_bonus,
-                    stealthiness_bonus,
-                    acrobatics_bonus,
-                    horse_riding_bonus,
-                    light_saber_fighting_bonus,
-                    hand_combat_bonus,
-                    usual_saber_fighting_bonus,
-                    evasion_bonus,
-                    throwing_bonus,
-                    lips_reading_bonus,
-                    old_weapon_bonus,
-                    light_weapon_bonus,
-                    heavy_weapon_bonus,
-            sniper_weapon_bonus,
-            musicality_bonus,
-            riding_bonus,
-            resisting_bonus,
-            strength_of_mind_bonus,
-            watchfulness_bonus,
-            pilot_bonus,
-            programming_bonus,
-            surviving_bonus,
-            reapair_bonus,
-            training_bonus,
-            medicine_crafting_bonus,
-            science_bonus,
-            engineering_bonus,
-            star_wars_tactic_bonus,
-            crafting_bonus,
-            trading_bonus,
-            astronavigation_bonus,
-            change_looking_bonus,
-            light_saber_crafting_bonus,
-            exploding_bonus,
-            security_bonus,
-            medicine_bonus,
-            language_bonus,
-            knowledge_bonus,
-            dancing_bonus,
-            interrogation_bonus,
-            diplomacy_bonus,
-            leadership_bonus,
-            singing_bonus,
-            deception_bonus,
-            insight_bonus,
-            seduction_bonus;
-
-
-
-        // SW_Character _SW_Char;      // Класс персонажа, с которым происходят изменения
+        
 
         private bool is_choosen;            // Флаг выбранного состояния
 
@@ -142,6 +89,7 @@ namespace Races_libs
         internal Race_class() 
         {
             Set_Error_Code(0);
+            Race_skill_bonus = new List<int>();
         }
         #endregion
 
@@ -272,79 +220,6 @@ namespace Races_libs
         public void Set_race_bonus_willpower(int insert_int) { race_bonus_willpower = insert_int; }
         // Предоставляем расовый бонус к атрибуту Сила воли
         public int Get_race_bonus_willpower() { return race_bonus_willpower; }
-        // Устанавливаем бонусы персонажа при выборе расы
-        /*
-        public void Set_race_bonus() 
-        {
-            foreach(Atribute_class atribute in _SW_Char._Atributes)
-            {
-                switch(atribute.Get_atribute_code())
-                {
-                    case (int)SW_Character.enum_Atributes.Strength:
-                        atribute.Increase_atr(Get_race_bonus_strength());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Agility:
-                        atribute.Increase_atr(Get_race_bonus_agility());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Stamina:
-                        atribute.Increase_atr(Get_race_bonus_stamina());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Quicness:
-                        atribute.Increase_atr(Get_race_bonus_quickness());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Perception:
-                        atribute.Increase_atr(Get_race_bonus_perception());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Intelligence:
-                        atribute.Increase_atr(Get_race_bonus_intelligence());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Charm:
-                        atribute.Increase_atr(Get_race_bonus_charm());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Willpower:
-                        atribute.Increase_atr(Get_race_bonus_willpower());
-                        break;
-                }
-            }
-        }
-        */
-        // Снимаем бонусы персонажа при смене расы
-        /*
-        public void Reset_race_bonus() 
-        {
-            foreach (Atribute_class atribute in _SW_Char._Atributes)
-            {
-                switch (atribute.Get_atribute_code())
-                {
-                    case (int)SW_Character.enum_Atributes.Strength:
-                        atribute.Decrease_atr(Get_race_bonus_strength());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Agility:
-                        atribute.Decrease_atr(Get_race_bonus_agility());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Stamina:
-                        atribute.Decrease_atr(Get_race_bonus_stamina());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Quicness:
-                        atribute.Decrease_atr(Get_race_bonus_quickness());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Perception:
-                        atribute.Decrease_atr(Get_race_bonus_perception());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Intelligence:
-                        atribute.Decrease_atr(Get_race_bonus_intelligence());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Charm:
-                        atribute.Decrease_atr(Get_race_bonus_charm());
-                        break;
-                    case (int)SW_Character.enum_Atributes.Willpower:
-                        atribute.Decrease_atr(Get_race_bonus_willpower());
-                        break;
-                }
-            }
-        }
-        */
-
         // Устанавливаем путь к текстовому файлу с описанием навыка
         public void Set_path_read_description(string input_text) { path_read_description = input_text; }
         // Предоставляем путь к текстовому файлу с описанием навыка
@@ -405,106 +280,6 @@ namespace Races_libs
         public int Get_race_watchfulness_combat_bonus() { return watchfulness_combat_bonus; }
         public void Set_race_flow_control_bonus(int insert_int) { flow_control_bonus = insert_int; }
         public int Get_race_flow_control_bonus() { return flow_control_bonus; }
-        public void Set_swimming_bonus(int insert_int) { swimming_bonus = insert_int; }
-        public int Get_swimming_bonus() { return swimming_bonus; }
-        public void Set_heavy_athletics_bonus(int insert_int) { heavy_athletics_bonus = insert_int; }
-        public int Get_heavy_athletics_bonus() { return heavy_athletics_bonus; }
-        public void Set_climbing_bonus(int insert_int) { climbing_bonus = insert_int; }
-        public int Get_climbing_bonus() { return climbing_bonus; }
-        public void Set_light_athletics_bonus(int insert_int) { light_athletics_bonus = insert_int; }
-        public int Get_light_athletics_bonus() { return light_athletics_bonus; }
-        public void Set_sleight_of_hand_bonus(int insert_int) { sleight_of_hand_bonus = insert_int; }
-        public int Get_sleight_of_hand_bonus() { return sleight_of_hand_bonus; }
-        public void Set_stealthiness_bonus(int insert_int) { stealthiness_bonus = insert_int; }
-        public int Get_stealthiness_bonus() { return stealthiness_bonus; }
-        public void Set_acrobatics_bonus(int insert_int) { acrobatics_bonus = insert_int; }
-        public int Get_acrobatics_bonus() { return acrobatics_bonus; }
-        public void Set_horse_riding_bonus(int insert_int) { horse_riding_bonus = insert_int; }
-        public int Get_horse_riding_bonus() { return horse_riding_bonus; }
-        public void Set_light_saber_fighting_bonus(int insert_int) { light_saber_fighting_bonus = insert_int; }
-        public int Get_light_saber_fighting_bonus() { return light_saber_fighting_bonus; }
-        public void Set_hand_combat_bonus(int insert_int) { hand_combat_bonus = insert_int; }
-        public int Get_hand_combat_bonus() { return hand_combat_bonus; }
-        public void Set_usual_saber_fighting_bonus(int insert_int) { usual_saber_fighting_bonus = insert_int; }
-        public int Get_usual_saber_fighting_bonus() { return usual_saber_fighting_bonus; }
-        public void Set_evasion_bonus(int insert_int) { evasion_bonus = insert_int; }
-        public int Get_evasion_bonus() { return evasion_bonus; }
-        public void Set_throwing_bonus(int insert_int) { throwing_bonus = insert_int; }
-        public int Get_throwing_bonus() { return throwing_bonus; }
-        public void Set_lips_reading_bonus(int insert_int) { lips_reading_bonus = insert_int; }
-        public int Get_lips_reading_bonus() { return lips_reading_bonus; }
-        public void Set_old_weapon_bonus(int insert_int) { old_weapon_bonus = insert_int; }
-        public int Get_old_weapon_bonus() { return old_weapon_bonus; }
-        public void Set_light_weapon_bonus(int insert_int) { light_weapon_bonus = insert_int; }
-        public int Get_light_weapon_bonus() { return light_weapon_bonus; }
-        public void Set_heavy_weapon_bonus(int insert_int) { heavy_weapon_bonus = insert_int; }
-        public int Get_heavy_weapon_bonus() { return heavy_weapon_bonus; }
-        public void Set_sniper_weapon_bonus(int insert_int) { sniper_weapon_bonus = insert_int; }
-        public int Get_sniper_weapon_bonus() { return sniper_weapon_bonus; }
-        public void Set_musicality_bonus(int insert_int) { musicality_bonus = insert_int; }
-        public int Get_musicality_bonus() { return musicality_bonus; }
-        public void Set_riding_bonus(int insert_int) { riding_bonus = insert_int; }
-        public int Get_riding_bonus() { return riding_bonus; }
-        public void Set_resisting_bonus(int insert_int) { resisting_bonus = insert_int; }
-        public int Get_resisting_bonus() { return resisting_bonus; }
-        public void Set_strength_of_mind_bonus(int insert_int) { strength_of_mind_bonus = insert_int; }
-        public int Get_strength_of_mind_bonus() { return strength_of_mind_bonus; }
-        public void Set_watchfulness_bonus(int insert_int) { watchfulness_bonus = insert_int; }
-        public int Get_watchfulness_bonus() { return watchfulness_bonus; }
-        public void Set_pilot_bonus(int insert_int) { pilot_bonus = insert_int; }
-        public int Get_pilot_bonus() { return pilot_bonus; }
-        public void Set_programming_bonus(int insert_int) { programming_bonus = insert_int; }
-        public int Get_programming_bonus() { return programming_bonus; }
-        public void Set_surviving_bonus(int insert_int) { surviving_bonus = insert_int; }
-        public int Get_surviving_bonus() { return surviving_bonus; }
-        public void Set_reapair_bonus(int insert_int) { reapair_bonus = insert_int; }
-        public int Get_reapair_bonus() { return reapair_bonus; }
-        public void Set_training_bonus(int insert_int) { training_bonus = insert_int; }
-        public int Get_training_bonus() { return training_bonus; }
-        public void Set_medicine_crafting_bonus(int insert_int) { medicine_crafting_bonus = insert_int; }
-        public int Get_medicine_crafting_bonus() { return medicine_crafting_bonus; }
-        public void Set_science_bonus(int insert_int) { science_bonus = insert_int; }
-        public int Get_science_bonus() { return science_bonus; }
-        public void Set_engineering_bonus(int insert_int) { engineering_bonus = insert_int; }
-        public int Get_engineering_bonus() { return engineering_bonus; }
-        public void Set_star_wars_tactic_bonus(int insert_int) { star_wars_tactic_bonus = insert_int; }
-        public int Get_star_wars_tactic_bonus() { return star_wars_tactic_bonus; }
-        public void Set_crafting_bonus(int insert_int) { crafting_bonus = insert_int; }
-        public int Get_crafting_bonus() { return crafting_bonus; }
-        public void Set_trading_bonus(int insert_int) { trading_bonus = insert_int; }
-        public int Get_trading_bonus() { return trading_bonus; }
-        public void Set_astronavigation_bonus(int insert_int) { astronavigation_bonus = insert_int; }
-        public int Get_astronavigation_bonus() { return astronavigation_bonus; }
-        public void Set_change_looking_bonus(int insert_int) { change_looking_bonus = insert_int; }
-        public int Get_change_looking_bonus() { return change_looking_bonus; }
-        public void Set_light_saber_crafting_bonus(int insert_int) { light_saber_crafting_bonus = insert_int; }
-        public int Get_light_saber_crafting_bonus() { return light_saber_crafting_bonus; }
-        public void Set_exploding_bonus(int insert_int) { exploding_bonus = insert_int; }
-        public int Get_exploding_bonus() { return exploding_bonus; }
-        public void Set_security_bonus(int insert_int) { security_bonus = insert_int; }
-        public int Get_security_bonus() { return security_bonus; }
-        public void Set_medicine_bonus(int insert_int) { medicine_bonus = insert_int; }
-        public int Get_medicine_bonus() { return medicine_bonus; }
-        public void Set_language_bonus(int insert_int) { language_bonus = insert_int; }
-        public int Get_language_bonus() { return language_bonus; }
-        public void Set_knowledge_bonus(int insert_int) { knowledge_bonus = insert_int; }
-        public int Get_knowledge_bonus() { return knowledge_bonus; }
-        public void Set_dancing_bonus(int insert_int) { dancing_bonus = insert_int; }
-        public int Get_dancing_bonus() { return dancing_bonus; }
-        public void Set_interrogation_bonus(int insert_int) { interrogation_bonus = insert_int; }
-        public int Get_interrogation_bonus() { return interrogation_bonus; }
-        public void Set_diplomacy_bonus(int insert_int) { diplomacy_bonus = insert_int; }
-        public int Get_diplomacy_bonus() { return diplomacy_bonus; }
-        public void Set_leadership_bonus(int insert_int) { leadership_bonus = insert_int; }
-        public int Get_leadership_bonus() { return leadership_bonus; }
-        public void Set_singing_bonus(int insert_int) { singing_bonus = insert_int; }
-        public int Get_singing_bonus() { return singing_bonus; }
-        public void Set_deception_bonus(int insert_int) { deception_bonus = insert_int; }
-        public int Get_deception_bonus() { return deception_bonus; }
-        public void Set_insight_bonus(int insert_int) { insight_bonus = insert_int; }
-        public int Get_insight_bonus() { return insight_bonus; }
-        public void Set_seduction_bonus(int insert_int) { seduction_bonus = insert_int; }
-        public int Get_seduction_bonus() { return seduction_bonus; }
         public void Set_small_img_path(string insert_text) { small_img_path = insert_text; Small_img_path = insert_text; }
         // Устанавливаем метод для вывода пути картинки каждой расы по запросу
         public string Get_small_img_path() { return small_img_path; }
