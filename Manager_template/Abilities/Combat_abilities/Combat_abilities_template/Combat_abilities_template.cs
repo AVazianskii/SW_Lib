b005aa3c28a991bd18c5e2da713bf62d95ee2cc6
AVazianskii <avazianskii@mail.ru>
2022-06-27 23:15:24 +0300

    added some privatte fields for combat abilities

--- Manager_template/Abilities/Combat_abilities/Combat_abilities_template/Combat_abilities_template.cs
+++ Manager_template/Abilities/Combat_abilities/Combat_abilities_template/Combat_abilities_template.cs
@@ -8,5 +8,23 @@ namespace SW_Character_creation
 {
     public class Combat_abilities_template : All_abilities_template
     {
+        private int reaction_bonus_base,
+                    reaction_bonus_adept,
+                    reaction_bonus_master,
+                    armor_bonus_base,
+                    armor_bonus_adept,
+                    armor_bonus_master,
+                    force_resistance_bonus_base,
+                    force_resistance_bonus_adept,
+                    force_resistance_bonus_master,
+                    stealthness_bonus_base,
+                    stealthness_bonus_adept,
+                    stealthness_bonus_master,
+                    watchfullness_bonus_base,
+                    watchfullness_bonus_adept,
+                    watchfullness_bonus_master,
+                    concentration_bonus_base,
+                    concentration_bonus_adept,
+                    concentration_bonus_master;
     }
 }
