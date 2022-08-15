017386f73ff0a6d779ff133b4ea28bfd648ce742
AVazianskii <avazianskii@mail.ru>
2022-08-15 21:59:48 +0300

    added class for description and managin a sequences of abilities with dependencies between abilities

--- Manager_template/Abilities/Abilities_sequence_template.cs
+++ Manager_template/Abilities/Abilities_sequence_template.cs
@@ -8,6 +8,88 @@ namespace SW_Character_creation
 {
     public class Abilities_sequence_template
     {
+        private All_abilities_template base_ability_lvl;
+        private All_abilities_template adept_ability_lvl;
+        private All_abilities_template master_ability_lvl;
+        private All_abilities_template enable_condition_base;
+        private All_abilities_template enable_condition_adept;
+        private All_abilities_template enable_condition_master;
 
+        private string name; // 
+
+        private bool is_enabled;
+
+
+
+        public All_abilities_template Base_ability_lvl
+        {
+            get { return base_ability_lvl; }
+            set { base_ability_lvl = value; }
+        }
+        public All_abilities_template Adept_ability_lvl
+        {
+            get { return adept_ability_lvl; }
+            set { adept_ability_lvl = value; }
+        }
+        public All_abilities_template Master_ability_lvl
+        {
+            get { return master_ability_lvl; }
+            set { master_ability_lvl = value; }
+        }
+        public All_abilities_template Enable_condition_base
+        {
+            get { return enable_condition_base; }
+            set { enable_condition_base = value; }
+        }
+        public All_abilities_template Enable_condition_adept
+        {
+            get { return enable_condition_adept; }
+            set { enable_condition_adept = value; }
+        }
+        public All_abilities_template Enable_condition_master
+        {
+            get { return enable_condition_master; }
+            set { enable_condition_master = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public bool Is_enabled
+        {
+            get { return is_enabled; }
+            set { is_enabled = value; }
+        }
+
+
+
+        public void Check_enable_state()
+        {
+            if ((enable_condition_base == null) || (enable_condition_base.Is_chosen))
+            {
+                Base_ability_lvl.Is_enable = true;
+            }
+            else { Base_ability_lvl.Is_enable = false; }
+
+            if ((enable_condition_adept == null) || (enable_condition_adept.Is_chosen))
+            {
+                Adept_ability_lvl.Is_enable = true;
+            }
+            else { Adept_ability_lvl.Is_enable = false; }
+
+            if ((enable_condition_master == null) || (enable_condition_master.Is_chosen))
+            {
+                Master_ability_lvl.Is_enable = true;
+            }
+            else { Master_ability_lvl.Is_enable = false; }
+        }
+
+
+
+        public Abilities_sequence_template()
+        {
+
+        }
     }
 }
