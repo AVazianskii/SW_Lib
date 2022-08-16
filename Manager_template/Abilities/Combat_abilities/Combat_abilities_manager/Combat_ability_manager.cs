cbe85b5f793438bf5814d90a31db8efbef098f5c
AVazianskii <avazianskii@mail.ru>
2022-08-16 23:50:09 +0300

    added configuring ability sequences for combat abilities. need test

--- Manager_template/Abilities/Combat_abilities/Combat_abilities_manager/Combat_ability_manager.cs
+++ Manager_template/Abilities/Combat_abilities/Combat_abilities_manager/Combat_ability_manager.cs
@@ -14,6 +14,12 @@ namespace SW_Character_creation
 
         private List<Combat_abilities_template> _Combat_abilities;
 
+        private Abilities_sequence_template echany,
+                                            mandolor_style,
+                                            zeishen,
+                                            matukai,
+                                            terras_kasi;
+
         private List<string> Combat_abilities_general_coloumn_name;
         private List<string> Combat_abilities_costs_coloumn_name;
         private List<string> Combat_abilities_combat_bonuses_coloumn_name;
@@ -37,10 +43,38 @@ namespace SW_Character_creation
             }
             return Combat_ability_manager_instance;
         }
+
+
+
+        public Abilities_sequence_template Echany
+        {
+            get { return echany; }
+            set { echany = value; }
+        }
+        public Abilities_sequence_template Mandolor_style
+        {
+            get { return mandolor_style; }
+            set { mandolor_style = value; }
+        }
+        public Abilities_sequence_template Zeishen
+        {
+            get { return zeishen; }
+            set { zeishen = value; }
+        }
+        public Abilities_sequence_template Matukai
+        {
+            get { return matukai; }
+            set { matukai = value; }
+        }
+        public Abilities_sequence_template Terras_kasi
+        {
+            get { return terras_kasi; }
+            set { terras_kasi = value; }
+        }
+
+
+
         public List<Combat_abilities_template> Get_abilities() { return _Combat_abilities; }
-
-
-
         public override void Run_download_and_upload_process()
         {
             SQLite_connection.Open();
@@ -89,6 +123,50 @@ namespace SW_Character_creation
                 _Combat_abilities[index].Concentration_bonus    = Combat_abilities_combat_bonuses[5][index];
 
             }
+
+            Echany.Base_ability_lvl     = _Combat_abilities[0];
+            Echany.Adept_ability_lvl    = _Combat_abilities[1];
+            Echany.Master_ability_lvl   = _Combat_abilities[2];
+
+            Echany.Enable_condition_base    = null;
+            Echany.Enable_condition_adept   = Echany.Base_ability_lvl;
+            Echany.Enable_condition_master  = Echany.Adept_ability_lvl;
+
+
+            Mandolor_style.Base_ability_lvl     = _Combat_abilities[3];
+            Mandolor_style.Adept_ability_lvl    = _Combat_abilities[4];
+            Mandolor_style.Master_ability_lvl   = _Combat_abilities[5];
+
+            Mandolor_style.Enable_condition_base    = null;
+            Mandolor_style.Enable_condition_adept   = Mandolor_style.Base_ability_lvl;
+            Mandolor_style.Enable_condition_master  = Mandolor_style.Adept_ability_lvl;
+
+
+            Zeishen.Base_ability_lvl    = _Combat_abilities[6];
+            Zeishen.Adept_ability_lvl   = _Combat_abilities[7];
+            Zeishen.Master_ability_lvl  = _Combat_abilities[8];
+
+            Zeishen.Enable_condition_base   = null;
+            Zeishen.Enable_condition_adept  = Zeishen.Base_ability_lvl;
+            Zeishen.Enable_condition_master = Zeishen.Adept_ability_lvl;
+
+
+            Matukai.Base_ability_lvl    = _Combat_abilities[9];
+            Matukai.Adept_ability_lvl   = _Combat_abilities[10];
+            Matukai.Master_ability_lvl  = _Combat_abilities[11];
+
+            Matukai.Enable_condition_base   = null;
+            Matukai.Enable_condition_adept  = Matukai.Base_ability_lvl;
+            Matukai.Enable_condition_master = Matukai.Adept_ability_lvl;
+
+
+            Terras_kasi.Base_ability_lvl    = _Combat_abilities[12];
+            Terras_kasi.Adept_ability_lvl   = _Combat_abilities[13];
+            Terras_kasi.Master_ability_lvl  = _Combat_abilities[14];
+
+            Terras_kasi.Enable_condition_base   = null;
+            Terras_kasi.Enable_condition_adept  = Terras_kasi.Base_ability_lvl;
+            Terras_kasi.Enable_condition_master = Terras_kasi.Adept_ability_lvl;
         }
 
 
@@ -112,6 +190,12 @@ namespace SW_Character_creation
             Combat_abilities_general_info.Add(Combat_abilities_name);
             Combat_abilities_general_info.Add(Combat_general_description);
             Combat_abilities_general_info.Add(Combat_description);
+
+            Echany          = new Abilities_sequence_template();
+            Mandolor_style  = new Abilities_sequence_template();
+            Zeishen         = new Abilities_sequence_template();
+            Matukai         = new Abilities_sequence_template();
+            Terras_kasi     = new Abilities_sequence_template();
 
             _Combat_abilities = new List<Combat_abilities_template>();
 
