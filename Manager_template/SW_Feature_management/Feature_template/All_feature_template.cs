fc6a9069024bbd4caeaa29cbab018e094d3c8cea
AVazianskii <avazianskii@mail.ru>
2022-10-28 13:21:02 +0300

    added new common claa for all features for new WPF page

---
+++ Manager_template/SW_Feature_management/Feature_template/All_feature_template.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW_Character_creation
+{
+    public class All_feature_template
+    {
+        private byte id,
+                     type;
+        private sbyte cost;
+
+        private string name;
+        private string description;
+
+        private bool is_chosen;
+
+
+
+        public byte ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public byte Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public sbyte Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public bool Is_chosen
+        {
+            get { return is_chosen; }
+            set { is_chosen = value; }
+        }
+
+
+
+        public virtual void Apply_feature_bonus()
+        {
+
+        }
+
+
+
+        public All_feature_template()
+        {
+
+        }
+
+    }
+}
