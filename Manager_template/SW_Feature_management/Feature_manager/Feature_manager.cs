265acd76c2d0ca175d2afe00f7f11616d047a2cd
AVazianskii <avazianskii@mail.ru>
2022-10-31 20:42:12 +0300

    Added feature manager

---
+++ Manager_template/SW_Feature_management/Feature_manager/Feature_manager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+
+namespace SW_Character_creation
+{
+    public class Feature_manager : Abstract_manager
+    {
+        private static Feature_manager feature_instance;
+
+
+        public static Feature_manager GetInstance()
+        {
+            if (feature_instance == null)
+            {
+                feature_instance = new Feature_manager();
+            }
+            return feature_instance;
+        }
+
+
+
+        private Feature_manager()
+        {
+
+        }
+    }
+}
